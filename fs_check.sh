fbcb73c34c2da120d08fbf01ac195d4a76ba0252
preethi k <kpreethiyadav579@gmail.com>
2022-07-18 16:43:49 +0530

    adding updata

--- fs_check.sh
+++ fs_check.sh
@@ -8,7 +8,8 @@ Filesystem_Path="/home/centos/POC/"
 dummy_fcheck()
 {
 echo "Tracking the dummy files in $Filesystem_Path:"
-find $(Filesystem_Path) -type  f \( -name '*test*' -o -name "*onetime*" -o -name "*dummy*" -o -name "*Test*file*" \) -exec ls -lt {} \; &>> $Filesystem_Path/fs_log.txt
+cd $Filesystem_Path
+find . -type  f \( -name '*test*' -o -name "*onetime*" -o -name "*dummy*" -o -name "*Test*file*" \) -exec ls -lt {} \; &>> $Filesystem_Path/fs_log.txt
 }
 
 old_dated_file()
@@ -25,3 +26,5 @@ echo "Finding out Top 5 High space usage files & listing <15days created files i
 dummy_fcheck
 old_dated_file
 highused_files
+
+exit 0;

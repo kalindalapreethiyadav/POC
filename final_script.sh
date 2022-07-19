5e69875874acd8623bbde4ac11dbb0077c363f4f
preethi k <kpreethiyadav579@gmail.com>
2022-07-19 15:49:01 +0530

    adding updata

--- final_script.sh
+++ final_script.sh
@@ -1,6 +1,9 @@
-1)dummy:-
-find . -type  f \( -name '*test*' -o -name "*onetime*" -o -name "*dummy*" -o -name "*Test*file*" \) -exec ls -lt {} \;
-2) >60 files in director:-
-find . -type f -mtime +1 -exec $(ls -lrt) {} \;
-3)top 5 files :-
-find . -type f -exec ls -al {} \; | sort -nr -k5 | head -n 5
+
+Filesystem_Path="/home/preethi/"
+echo "Tracking the dummy files in $Filesystem_Path:"
+find . -type  f \( -name '*test*' -o -name "*onetime*" -o -name "*dummy*" -o -name "*Test*file*" \) -exec ls -lt {} \; &>> /home/preek/logfile.txt
+echo "Tracking the >60days old dated files in $Filesystem_Path:"
+find . -type f -mtime +1 -exec $(ls -lrt) {} \; &>> /home/preek/logfile.txt
+echo "Finding out Top 5 High space usage files & listing <15days created files in $Filesystem_Path:"
+find . -type f -exec ls -al {} \; | sort -nr -k5 | head -n 5; &>> /home/preethi/logfile.txt
+echo "******sucessful*******"

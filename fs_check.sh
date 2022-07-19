e7adcd4ffb42d597805e225ee18059e64ee8509a
preethi k <kpreethiyadav579@gmail.com>
2022-07-19 13:02:27 +0530

    adding updata

--- fs_check.sh
+++ fs_check.sh
@@ -15,11 +15,17 @@ find . -type  f \( -name '*test*' -o -name "*onetime*" -o -name "*dummy*" -o -na
 old_dated_file()
 {
 echo "Tracking the >60days old dated files in $Filesystem_Path:"
+cd $Filesystem_Path
+find . -type f -mtime +1 -exec $(ls -lrt) {} \;
 }
 
 highused_files()
 {
 echo "Finding out Top 5 High space usage files & listing <15days created files in $Filesystem_Path:"
+echo "checking test 1:"
+find . -type f -exec ls -al {} \; | sort -nr -k5 | head -n 5
+echo "checking test 2:"
+find . -type f -exec ls -s {} \; | sort -nr | awk 'NR==1 { $1=""; sub(/^ /, ""); print }'
 }
 
 #calling functions

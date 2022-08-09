2e177654df95920a3bf4b6318e2af1bcafb08b94
preethi k <kpreethiyadav579@gmail.com>
2022-08-09 17:27:49 +0530

    adding update

--- pfile _code.sh
+++ pfile _code.sh
@@ -7,7 +7,7 @@ fsize="+200c"
 
 pre_fcheck()
  {
-echo -e "\e[32mPre-defined test files in $1:\n \e[0m"
+echo -e "\e[32m Test files in $1:\n \e[0m"
 find $1 -type f \( -name '*test*' -o -name "*onetime*" -o -name "*dummy*" -o -name "*TEST*FILE*" -o -name "*test*file*" -o -name '*ONETIME*' -o -name '*TEST*' -o -name '*DUMMY*' \) -exec ls -lt {} \; | awk '{print $NF}'
 echo "********************************************************"
  } >> /home/preek/pre_definedfiles.txt
@@ -22,7 +22,7 @@ echo "********************************************************"
 
 highused_files()
 {
-echo -e "\nFiles size >2GB & modified 60 days before details in $1 :"
+echo -e "\nFiles size > $fsize & modified 60 days before details in $1 :"
 find $1 -type f -mtime +60 -size $fsize -ls | awk '{print $(NF-4)" "$NF}'
 echo "********************************************************"
 } >> /home/preek/highused_files.txt

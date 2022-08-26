21d294aaeb36e7151e4b42c840b7d05cce745911
preethi k <kpreethiyadav579@gmail.com>
2022-08-26 18:19:30 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -5,10 +5,28 @@ exec 1> file_tracking.log 2>&1
 filepath=$(cat filepath.txt)
 fsize="+200c"
 
-level2_track()
+level2_ftrack()
 {
 cd $1
-for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -5 | awk '{print $NF}')
+for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}')
+{
+    if [ -d "$line" ] ; then
+        echo "director name : $line";
+    else
+        if [ -f "$line" ]; then
+            echo "$line is a file";
+        else
+            echo "$line no files or directors found";
+            exit 1
+        fi
+    fi
+}
+}
+
+level3_dftrack()
+{
+cd $1
+for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}')
 {
     if [ -d "$line" ] ; then
         echo "director name : $line";

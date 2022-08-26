8be760d969613b766e43166fbc9c7632c7846303
preethi k <kpreethiyadav579@gmail.com>
2022-08-26 17:07:58 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -5,22 +5,7 @@ exec 1> file_tracking.log 2>&1
 filepath=$(cat filepath.txt)
 fsize="+200c"
 
-file_track()
- {
-echo -e "\e[32m Test files in $1:\n \e[0m"
-find $1 -type f \( -name '*test*' -o -name "*onetime*" -o -name "*dummy*" -o -name "*TEST*FILE*" -o -name "*test*file*" -o -name '*ONETIME*' -o -name '*TEST*' -o -name '*DUMMY*' \) -exec ls -lt {} \; | awk '{print $NF}'
-echo -e "\e[34m-------------------------------------------\e[0m"
-
-echo -e "\e[32m Files older than 60 days in $1 :\n \e[0m"
-find $1 -type f -mtime +60 -ls | awk '{print $(8)" "$(9) "\t" $(NF-4) "\t" $NF}'
-echo -e "\e[34m-------------------------------------------\e[0m"
-
-echo -e "\e[32m Files size > $fsize & modified 15 days prior in $1 : \n \e[0m"
-find $1 -type f -mtime +15 -size $fsize -ls | awk '{print $(NF-4)" "$NF}'
-echo -e "\e[34m-------------------------------------------\e[0m"
- }
-
-High_used_file()
+level2_track()
 {
 cd $1
 for line in  (find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -5)
@@ -37,12 +22,6 @@ for line in  (find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -r
     fi
 }
 
-director_func()
-{
-for line in (find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -5)
- 
-}
-
 for line in $filepath
 do
 echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
@@ -57,12 +36,10 @@ done
 
 for line in $filepath
 do
- total_size=$(df -g $line | awk '{print $2}')
+ total_size=$(df $line | awk '{print $2}')
  echo $total_size
- director_func $line
+ level2_track $line
  done
-
-
 
 echo -e "\e[32m *******Succesfully completed***********\n \e[0m"
 exit 0

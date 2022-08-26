17b02c7ca9725c7380bba612c9804724611b8904
preethi k <kpreethiyadav579@gmail.com>
2022-08-26 17:51:11 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -8,7 +8,8 @@ fsize="+200c"
 level2_track()
 {
 cd $1
-for line in ${(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -5)}
+#varibale = $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -5)
+for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -5)
 {
     if [ -d "$line" ] ; then
         echo "director name : $line";

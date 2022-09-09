04e3d005f0eb81963012f02a51687c9a7d3cbca1
preethi k <kpreethiyadav579@gmail.com>
2022-09-09 11:19:09 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -17,14 +17,11 @@ for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -r
             used_fsize=$(ls -lrt $line | awk '{print $5F}')
             echo $total_size $used_fsize 
             fpercent=$((100*$used_fsize/$total_size ))
-                if [ $fpercent -ge 40 ]
-                {
-                    echo "$line is > 40%"
-                }
-                else {
-                    echo " " > /dev/null
+                if [[ $(fpercent) -ge 40 ]] ; then
+                echo -e 'This $line file is 40% greater\n'
+                else
+                echo 'This $line is not large enough'
                 fi
-                }
         else
             echo " " > /dev/null
         fi

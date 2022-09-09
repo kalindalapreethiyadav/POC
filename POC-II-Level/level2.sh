2bed4881395b7176ec9f6202e2668a10b8b54a13
preethi k <kpreethiyadav579@gmail.com>
2022-09-09 13:37:01 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -6,6 +6,8 @@ filepath=$(cat filepath.txt)
 #fsize="+200c"
 file_per=1
 
+echo -e "\e[32m *******Script started on $date ***********\n \e[0m" 
+## echo the date at start
 level2_ftrack()
 {
 echo -e "\e[32m Tracking files > $file_per in $1:\n \e[0m"
@@ -54,6 +56,6 @@ do
 level2_ftrack $line
 done
 
-echo -e "\e[32m *******Succesfully completed***********\n \e[0m"
+echo -e "\e[32m *******Succesfully completed on $date ***********\n \e[0m"
 exit 0
 

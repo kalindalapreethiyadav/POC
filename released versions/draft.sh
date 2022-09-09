ee7aa73df0b149d7f735dab77235364b02dd26db
preethi k <kpreethiyadav579@gmail.com>
2022-09-09 13:42:19 +0530

    adding

--- released versions/draft.sh
+++ released versions/draft.sh
@@ -10,7 +10,7 @@ echo -e "\e[32m *******Script started on $date ***********\n \e[0m"
 ## echo the date at start
 level2_ftrack()
 {
-echo -e "\e[32m Tracking files > $file_per in $1:\n \e[0m"
+echo -e "\e[32m Tracking files > $file_per% in $1:\n \e[0m"
 total_size=$(df $line | awk '{print $2}' | tail -1)
 cd $line
 for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}')

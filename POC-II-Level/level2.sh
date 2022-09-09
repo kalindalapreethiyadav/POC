467df7905f72076d9f5de11bc6638afab789ddea
preethi k <kpreethiyadav579@gmail.com>
2022-09-09 11:57:03 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -16,7 +16,7 @@ for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -r
         if [ -f "$line" ]; then
            # echo -e "$line is a file";
             used_fsize=$(ls -lrt $line | awk '{print $5F}')
-            echo $total_size $used_fsize 
+            #echo $total_size $used_fsize 
             fpercent=$((100*$used_fsize/$total_size ))
                 if [ $fpercent -ge $fper ] ; then
                 echo "$line file > $fper %"

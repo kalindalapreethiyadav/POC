34c247275d00ac3495713a6fe9803706f556ac91
preethi k <kpreethiyadav579@gmail.com>
2022-09-16 15:12:52 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -4,14 +4,14 @@
 filepath=$(cat filepath.txt)
 #fsize="+200c"
 file_per=1
-echo -e "\e[32m *******Script started on $date ***********\n \e[0m" 
+echo -e "\e[32m *******Script started on $(date +%F) ***********\n \e[0m" 
 ## echo the date at start
 level2_ftrack()
 {
 echo -e "\e[32m Tracking files > $file_per% in $1:\n \e[0m"
 total_size=$(df $line | awk '{print $2}' | tail -1)
 cd $line
-for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} 2>/dev/null + | sort -rn | head -10 | awk '{print $NF}')
+for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}')
         {
         if [ -f "$line" ]; then
            # echo -e "$line is a file";
@@ -56,6 +56,6 @@ do
 level2_ftrack $line
 done
 
-echo -e "\e[32m *******Succesfully completed on $date ***********\n \e[0m"
+echo -e "\e[32m *******Succesfully completed on $(date +%F) ***********\n \e[0m"
 exit 0
 

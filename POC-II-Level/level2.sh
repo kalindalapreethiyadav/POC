b1a7013d6c9598cbcd3d783038c1522d335d75f8
preethi k <kpreethiyadav579@gmail.com>
2022-09-19 19:02:14 +0530

    adding

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -12,7 +12,7 @@ level2_ftrack()
 {
 echo -e "\e[32m Tracking files > $file_per% in $1:\n \e[0m"
 total_size=$(df $line | awk '{print $2}' | tail -1)
-t_files=$(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}' 1> list_files.txt 2>/dev/null)
+t_files=$(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}' 1>list_files.txt 2>/dev/null)
 cd $line
 for line in $tracked_files
         {

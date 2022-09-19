07b16e8e327bde96987a43dd65c41ec3d4d49852
preethi k <kpreethiyadav579@gmail.com>
2022-09-19 15:52:23 +0530

    adding

--- POC-II-Level/dir_lev3.sh
+++ POC-II-Level/dir_lev3.sh
@@ -3,27 +3,25 @@
 filepath=$(cat filepath.txt)
 #fsize="+200c"
 file_per=1
-dir_per=10
+dir_per=5
 echo -e "\e[32m *******Script started on $(date +'%m/%d/%Y') ***********\n \e[0m" 
 ## echo the date at start
 
 level3_dftrack()
 {
-echo -e "\e[32m Tracking directries > $file_per% in $1:\n \e[0m"
+echo -e "\e[32m Tracking directries > $dir_per% in $1:\n \e[0m"
 total_size=$(df $line | awk '{print $2}' | tail -1)
-cd $line
-ls -lrt
+cd $1
 for line in $(find . -path '*/\.*' -prune -o -type d -exec du -sk {} + | sort -rn | head -5 | awk '{print $NF}')
         {
         if [ -d "$line" ]; then
-           #echo -e "$line is a file";
-            used_fsize=$(ls -lrt $line | awk '{print $5F}')
-            #echo $total_size $used_fsize 
+            used_dsize=$(ls -lrt $line | awk '{print $5F}') 
             dpercent=$((100*$used_fsize/$total_size ))
+            echo "Total_SIZE = $total_size Used_SIZE = $used_dsize  directory_Details = $line"
                 if [ $dpercent -ge $dir_per ] ; then
-                    echo "$line is greater than 10%\n"
+                    echo "$line is greater than 5%\n"
                 else
-                echo "Director $line is not > 10%"
+                echo "Director $line is not > 5%"
                 fi
         else
             echo " " > /dev/null

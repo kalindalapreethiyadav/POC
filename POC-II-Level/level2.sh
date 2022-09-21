f321ca4f391db7ab31d6dd01f20aee438ef6c218
preethi k <kpreethiyadav579@gmail.com>
2022-09-21 23:12:14 +0530

    addingupdate

--- POC-II-Level/level2.sh
+++ POC-II-Level/level2.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-#Tracking files >5% based on Total_size of Disk Filesystem
-exec 1> file_tracking.log 2>&1  #Saving entire log in single file
-filepath=$(cat filepath.txt) #Provide the Paths where we need to the Track files
-Track_file_percent=5     # Identifing the files > 5%
+<<Description
+*****************************************************************************************
+Tracking & Identifing only High Disk space consumed files in all directories recursively on the Filesystem Paths
+******************************************************************************************
+Description
+
+exec 1> file_tracking.log 2>&1  
+filepath=$(cat /home/preek/filepath.txt) #Providing the Filesystem Paths where to Track files
+Track_file_percent=5  #Providing "%" that how much we need to track based on FS.
 
 echo -e "\e[35m *******Script started on $(date)***********\n \e[0m"
 
 level2_ftrack()
 {
 echo -e "\e[32m Tracking files > $Track_file_percent% in $1:\n \e[0m"
+#Finding the Total size of the fileystem
 total_size=$(df $line | awk '{print $2}' | tail -1)
 cd $line
 
-#Identifying the files in the Filesystem Path by ignoring the Hidden-binary files and Tracking HIGH DISK USED files by sorting and filtering the TOP 10 high disk used files in specified Paths
+#Tracking & identifying Top 10 High disk consumed files only on all directories(including sub -dir) recursivly in the Filesystem Path
 
 for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}')
         do
         if [ -f "$line" ]; then
-           # echo -e "$line is a file";
             used_fsize=$(ls -lrt $line | awk '{print $5F}')
-            #echo $total_size $used_fsize
             fpercent=$((100*$used_fsize/$total_size ))
                 if [ $fpercent -ge $Track_file_percent ] ; then
                 echo "Total_SIZE = $total_size Used_SIZE = $used_fsize Used_percent = $fpercent  File_Details = $line"

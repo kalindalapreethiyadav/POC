3b9a3ff2f7db5afa40e60887b0efafd6a538820c
preethi k <kpreethiyadav579@gmail.com>
2022-10-13 19:50:52 +0530

    adding

---
+++ POC-II-Level/smart_HighDisk_FileTracker_D.sh
@@ -0,0 +1,97 @@
+
+
+#!/bin/bash
+
+<<Description
+*****************************************************************************************
+Tracking & Identifing only High Disk space consumed files in all directories recursively on the Filesystem Paths
+******************************************************************************************
+Description
+
+exec 1> file_tracking.log 2>&1
+filepath=$(cat /home/preek/filepath.txt) #Providing the Filesystem Paths where to Track files
+FS_Scratch=$(cat FSpaths_Scratch.txt)
+FS_DS_CLA=$(cat FSpaths_Datasets_CLA.txt)
+FS_DS=$(cat FSpaths_Datasets.txt)
+FS_DS_ISOMS=$(cat FSpaths_Datasets_ISOMS.txt)
+FS_PP_ISOM=$(cat FSpaths_PP_ISOM.txt)
+FS_PP=$(cat FSpaths_PP.txt)
+FS_P_DS=$(cat FSpaths_P_DS.txt)
+FS_DS_IDSS=$(cat FSpaths_Datasets_IDSS.txt)
+FS_PP_CTS=$(cat FSpaths_PP_CTS.txt)
+
+
+Track_file_percent=40 #Providing "%" that how much we need to track based on FS.
+
+echo -e "\e[35m *******Script started on $(date)***********\n \e[0m"
+
+level2_ftrack()
+{
+echo -e "\e[32m Tracking files > $Track_file_percent% in $1:\n \e[0m"
+#Finding the Total size of the fileystem
+total_size=$(df $line | awk '{print $2}' | tail -1)
+cd $line
+
+#Tracking & identifying Top 10 High disk consumed files only on all directories(including sub -dir) recursivly in the Filesystem Path
+
+for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}')
+        do
+        if [ -f "$line" ]; then
+            used_fsize=$(ls -lrt $line | awk '{print $5F}')
+            fpercent=$((100*$used_fsize/$total_size ))
+                if [ $fpercent -ge $Track_file_percent ] ; then
+                echo "Total_SIZE = $total_size Used_SIZE = $used_fsize Used_percent = $fpercent  File_Details = $line"
+                else
+                echo " " > /dev/null #supress the data
+                fi
+        else
+            echo " " > /dev/null
+        fi
+        done
+    }
+
+FS_Scratch=$(cat FSpaths_Scratch.txt)
+FS_DS_CLA=$(cat FSpaths_Datasets_CLA.txt)
+FS_DS=$(cat FSpaths_Datasets.txt)
+FS_DS_ISOMS=$(cat FSpaths_Datasets_ISOMS.txt)
+FS_PP_ISOM=$(cat FSpaths_PP_ISOM.txt)
+FS_PP=$(cat FSpaths_PP.txt)
+FS_P_DS=$(cat FSpaths_P_DS.txt)
+FS_DS_IDSS=$(cat FSpaths_Datasets_IDSS.txt)
+FS_PP_CTS=$(cat FSpaths_PP_CTS.txt)
+
+for line in $filepath 
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line 
+done
+
+for line in $FS_Scratch 
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
+for line in $FS_DS_CLA 
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
+for line in $FS_DS 
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
+for line in $FS_DS_ISOMS 
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
+
+
+echo -e "\e[35m *******Succesfully completed on $(date) ***********\n \e[0m"
+exit 0
+
--- POC-II-Level/smart_HighDisk_FileTracker_P.sh
+++ POC-II-Level/smart_HighDisk_FileTracker_P.sh
@@ -1,3 +1,5 @@
+
+
 #!/bin/bash
 
 <<Description
@@ -6,9 +8,14 @@ Tracking & Identifing only High Disk space consumed files in all directories rec
 ******************************************************************************************
 Description
 
-exec 1> file_tracking.log 2>&1  
-filepath=$(cat /home/preek/filepath.txt) #Providing the Filesystem Paths where to Track files
-Track_file_percent=3  #Providing "%" of File_Size that how much we need to track based on FS.
+exec 1> file_tracking.log 2>&1
+FS_Scratch=$(cat FSpaths_Scratch.txt)
+FS_DS_CLA=$(cat FSpaths_Datasets_CLA.txt)
+FS_DS=$(cat FSpaths_Datasets.txt)
+FS_DS_ISOMS=$(cat FSpaths_Datasets_ISOMS.txt)
+FS_DS_IDSS=$(cat FSpaths_Datasets_IDSS.txt)
+
+Track_file_percent=40 #Providing "%" that how much we need to track based on FS.
 
 echo -e "\e[35m *******Script started on $(date)***********\n \e[0m"
 
@@ -20,13 +27,13 @@ total_size=$(df $line | awk '{print $2}' | tail -1)
 cd $line
 
 #Tracking & identifying Top 10 High disk consumed files only on all directories(including sub -dir) recursivly in the Filesystem Path
-set +e
-for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn |head -10  | awk '{print $NF}')
+
+for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -rn | head -10 | awk '{print $NF}')
         do
         if [ -f "$line" ]; then
             used_fsize=$(ls -lrt $line | awk '{print $5F}')
             fpercent=$((100*$used_fsize/$total_size ))
-                if [ $fpercent -ge $Track_file_percent && ] ; then
+                if [ $fpercent -ge $Track_file_percent ] ; then
                 echo "Total_SIZE = $total_size Used_SIZE = $used_fsize Used_percent = $fpercent  File_Details = $line"
                 else
                 echo " " > /dev/null #supress the data
@@ -36,15 +43,46 @@ for line in $(find . -path '*/\.*' -prune -o -type f -exec du -sk {} + | sort -r
         fi
         done
     }
-set -e
 
-for line in $filepath #Reading each filesystem path in filepath file
+for line in $FS_Scratch 
 do
 echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
-level2_ftrack $line #Passing with argument to function
+level2_ftrack $line 
 done
+
+sleep 5
+
+for line in $FS_DS_CLA
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
+sleep 5
+
+for line in $FS_DS 
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
+sleep 5
+
+for line in $FS_DS_ISOMS
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
+sleep 5
+
+for line in $FS_DS_IDSS
+do
+echo -e "\e[36m****************started files tracking on $line******************\e[0m \n"
+level2_ftrack $line
+done
+
 
 echo -e "\e[35m *******Succesfully completed on $(date) ***********\n \e[0m"
 exit 0
-
 
